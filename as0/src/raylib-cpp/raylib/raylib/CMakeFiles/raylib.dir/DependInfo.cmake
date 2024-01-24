034c2ff3f95df6e7bbc2e37e9d2b776c56273686
masonhaines <mhaines@nevada.unr.edu>
2024-01-24 14:31:01 -0800

    as0 first commit

---
+++ as0/src/raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/cs381/as0/raylib-cpp/raylib/src/raudio.c" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/raudio.c.o" "gcc" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/raudio.c.o.d"
+  "/mnt/c/cs381/as0/raylib-cpp/raylib/src/rcore.c" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/rcore.c.o" "gcc" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/rcore.c.o.d"
+  "/mnt/c/cs381/as0/raylib-cpp/raylib/src/rmodels.c" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/rmodels.c.o" "gcc" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/rmodels.c.o.d"
+  "/mnt/c/cs381/as0/raylib-cpp/raylib/src/rshapes.c" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/rshapes.c.o" "gcc" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/rshapes.c.o.d"
+  "/mnt/c/cs381/as0/raylib-cpp/raylib/src/rtext.c" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/rtext.c.o" "gcc" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/rtext.c.o.d"
+  "/mnt/c/cs381/as0/raylib-cpp/raylib/src/rtextures.c" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/rtextures.c.o" "gcc" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/rtextures.c.o.d"
+  "/mnt/c/cs381/as0/raylib-cpp/raylib/src/utils.c" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/utils.c.o" "gcc" "raylib-cpp/raylib/raylib/CMakeFiles/raylib.dir/utils.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/cs381/as0/src/raylib-cpp/raylib/raylib/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ as0/src/raylib-cpp/raylib/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/cs381/as0/raylib-cpp/raylib/src
+# Build directory: /mnt/c/cs381/as0/src/raylib-cpp/raylib/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
