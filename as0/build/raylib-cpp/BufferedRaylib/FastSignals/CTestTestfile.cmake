c6b7b82de967bc461b952944d93a7dd93e2667f3
masonhaines <mhaines@nevada.unr.edu>
2024-01-30 08:52:23 -0800

    new gitignore, remove build contents

--- as0/build/raylib-cpp/BufferedRaylib/FastSignals/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/UNR CSE/cs381/as0/raylib-cpp/BufferedRaylib/FastSignals
-# Build directory: /mnt/c/UNR CSE/cs381/as0/build/raylib-cpp/BufferedRaylib/FastSignals
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libfastsignals")
--- as0/build/raylib-cpp/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/UNR CSE/cs381/as0/raylib-cpp/raylib
-# Build directory: /mnt/c/UNR CSE/cs381/as0/build/raylib-cpp/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
