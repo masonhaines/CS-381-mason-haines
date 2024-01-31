582f5ab6846d58609433b2c736d30a6d65699e1c
masonhaines <mhaines@nevada.unr.edu>
2024-01-30 16:20:18 -0800

    added pngs

--- as0/src/as0.cpp
+++ as0/src/as0.cpp
@@ -4,21 +4,38 @@
 
 using namespace std;
 
-void textFun (int& red, int& green, int& blue);
+//function prototype
+void textFun (int& red, int& green, int& blue);//passing by reference 
+
+//make 2D texture object
+Texture2D mask;
+Texture2D rupee;
 
 int main() {
 
-    raylib::Window window(800, 800, "CS381 - Assignment 0");
-    raylib::Color colors;
-    raylib::Text text;
+    raylib::Window window(800, 800, "CS381 - Assignment 0");//initialize window 
+    raylib::Color colors;//colors object 
+    raylib::Text text;//text object 
 
+    //load image as texture into texture object mask 
+    mask = LoadTexture("/mnt/c/UNR CSE/cs381/as0/majorasMask.png");
+    rupee = LoadTexture("/mnt/c/UNR CSE/cs381/as0/green_rupee_small.png");
+
+    //make window resizable
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+
+    //create RGBA values and initialize 
     int red = 0;
     int green = 0;
     int blue = 0; 
     int alpha = 255;
+    
+    float textSize = 1;//TEXT SIZE 
+    const char *Label = {"Oi! ZELDA! Get out of my sandwich!"};
 
-    float textSize = 1;
-    const char *Label = {"I'm coming to get yah!"};
+    //variable to change image POS
+    int BOUNCE = 0;
+    bool bounceFlag = true;
 
     
     
@@ -27,39 +44,67 @@ int main() {
         int height = window.GetHeight();
         int width = window.GetWidth();
 
-        red += 3;
-        green += 3;
-        blue += 3;
-        textSize += .05;
-        
-        // if (textSize >= 10) {
-        //     textSize += 1;
-        // }
-        if (textSize >= 30) {
+        /**************************************************/
+        //This is to make those rupees bounce!!
+        if (bounceFlag) {
+            BOUNCE++;
+        } else {
+            BOUNCE--;
+        }
+
+        if (BOUNCE == 250) {
+            bounceFlag = false;
+        } else if (BOUNCE == -250) {
+            bounceFlag = true;
+        }
+        /**************************************************/
+
+        //increment text size and and RGB values 
+        red += 4;
+        green += 1;
+        blue += 11;
+        textSize += .025;
+        //reset text size as it gets to 30 
+        if (textSize >= 45) {
             textSize = 10;
         }
        
-        textFun(red, green, blue);
+        textFun(red, green, blue);//check if RGB values need to be reset
 
-        window.BeginDrawing();
+        window.BeginDrawing();//begin drawing inside window 
         {
+            //as window size is adjusted the colors of the window will change 
             if (width % 2 != 0 || height % 2 != 0) {
-                window.ClearBackground(colors.Yellow());
-            }else {
-                window.ClearBackground(colors.Green());
+                window.ClearBackground(colors.Black());
+            } else if (width % 3 != 0 || height % 3 != 0){
+                window.ClearBackground(colors.DarkPurple());
+            } else if (width % 4 != 0 || height % 4 != 0){
+                window.ClearBackground(colors.Red());
             }
             
-            text.Draw(Label, ((width * .5) - (MeasureText(Label, textSize) * .5)), .5 * height, textSize, raylib::Color{red,green,blue,alpha});
-            // const Vector2 &position, raylib::Radian rotation, const Vector2 &origin = {(0), (0)}) const
-            // text.Draw((1,1), raylib::Radian(2.75), {(0), (0)});
-            // text.Draw()
-            
+            //make a bouncing rupee
+            Rectangle rupeeSource = (Rectangle){0, 0, 200, 200};
+            // I dont like to wrap gifts or functions correctly //
+            /**/Rectangle rupeeDest = (Rectangle){static_cast<float>(width / 2) - (rupeeSource.width) / 2, 
+            /**/static_cast<float>(height / 2) - ((rupeeSource.height) / 10), 
+            /**/rupeeSource.width, rupeeSource.height};
+            DrawTexturePro(rupee, rupeeSource, rupeeDest, (Vector2){BOUNCE, 0}, 0, WHITE);
+
+            //Make a rotating majoras mask 
+            Rectangle maskSource = (Rectangle){0, 0, 675, 593};
+            Rectangle maskDest = (Rectangle){static_cast<float>(width / 2), static_cast<float>(height / 2), maskSource.width, maskSource.height};
+            DrawTexturePro(mask, maskSource, maskDest, (Vector2){0, 0}, 45 * GetTime(), RED);
+
+            //Create a text
+            //gets larger over time based off of textsize incrementing and RGB values are changing colors
+            text.Draw(Label, ((width / 2) - (MeasureText(Label, textSize) / 2)), height / 2, textSize, raylib::Color{red,green,blue,alpha});
         }
 
         window.EndDrawing();
     }
 };
 
+//function made to change colors of the text that is using RGB colors 
 void textFun (int& red, int& green, int& blue){
     if (red == 245) {
         red = 0;
@@ -71,3 +116,5 @@ void textFun (int& red, int& green, int& blue){
         blue = 0;
     }
 }
+    
+    
