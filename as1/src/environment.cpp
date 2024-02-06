976fef2ffd4e3ddfefd15dd74f849c8c1bbbf839
masonhaines <mhaines@nevada.unr.edu>
2024-02-05 21:30:34 -0800

    Tried loading a model
    tried loading a model but am gonna need to figure out the shader or texture.

--- as1/src/environment.cpp
+++ as1/src/environment.cpp
@@ -1,20 +1,30 @@
 #include "raylib-cpp.hpp"
 #include "rlgl.h"
+#include "skybox.hpp"
 
+// Some object that can be used as a function, and takes a transform in, and returns a transform 
+template<typename T>
+concept Transformer = requires( T t, raylib:: Transform m) {
+    {t.operator()(m) } -> std::convertible_to<raylib::Transform>;
+};
+
+void DrawBoundedModel(raylib::Model& model, Transformer auto transformer) {
+    raylib::Transform backupTransform = model.transform; // save the models transform 
+    model.transform = transformer(backupTransform); // update the models transform based on our tranform function
+    model.Draw({});
+    model.GetTransformedBoundingBox().Draw();
+    model.transform = backupTransform;
+}
 
 int main() {
 
     raylib::Window window(800, 800, "CS381 - Assignment 1");//initialize window 
     
-    raylib::Model bad("bad.obj");
-    
-
+    // raylib::Model bad("bad.obj");
 
     //***************************************************//
     raylib::Camera camera(
-        // raylib::Vector3(0, 120, -500),
-        // raylib::Vector3(0, 0, 300), 
-        raylib::Vector3(0, 0, -10),
+        raylib::Vector3(3, 5, 15),
         raylib::Vector3(0, 0, 0),
         raylib::Vector3::Up(), 
         45, 
@@ -23,13 +33,21 @@ int main() {
     //**************************************************//
     raylib::Text text;//text object 
 
-    // skyBox skyBox;
-    // skyBox.Load("../textures/skybox.png");
+    cs381::SkyBox skyBox("../textures/skybox.png");
     
-    raylib::Model defaultCube("bad.obj");
+    // raylib::Model defaultCube("bad.obj");
 
-    float speed = 10;
-    Vector3 position = {0, 0, 0};
+    // raylib::Mesh UploadMesh
+
+    raylib::Model oilTanker = LoadModel("../meshes/OilTanker.glb");
+    // oilTanker.Load("../meshes/OilTanker.glb");
+
+    Texture2D texture = LoadTexture("../textures/skybox.png");
+    oilTanker.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
+
+
+    // float speed = 10;
+    // Vector3 position = {0, 0, 0};
     
     while(!window.ShouldClose()){
 
@@ -38,21 +56,30 @@ int main() {
 
         window.BeginDrawing();//begin drawing inside window 
         {
-            window.ClearBackground(RAYWHITE);
-            text.Draw("Hello World", 0, 100, 20, raylib::Color::DarkPurple());
+            // window.ClearBackground(PINK);
+            // text.Draw("Hello World", 0, 100, 20, raylib::Color::DarkPurple());
         
-            // camera.BeginMode();
-            // {
-            //     // defaultCube.Draw({0, 0, 0});
-            //     // skyBox.Draw();
-            // }
-            // camera.EndMode();
+            camera.BeginMode();
+            {
+                // DrawBoundedModel(oilTanker, [](raylib::Transform t) -> raylib::Transform {
+                //     return t.Translate({0, 1, 0}).RotateZ(raylib::Degree(45));
+                // });
+                DrawModel(oilTanker, Vector3{0,5,0}, 5, WHITE);
+                
+
+
+                // skyBox.Draw();
+                // defaultCube.Draw({0, 0, 0});
+            }
+            camera.EndMode();
             
             // position += raylib::Vector3::Right() * speed * window.GetFrameTime();
         }
         window.EndDrawing();
 
     }
+    UnloadTexture(texture);
+    UnloadModel(oilTanker);
 };
 
 
