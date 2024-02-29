079d7c7f32128bd7d9e42ef4331513140c888c85
masonhaines <mhaines@nevada.unr.edu>
2024-02-29 11:03:07 -0800

    figured out how to remove and reset rupee
    need to have four parameters to reset rupee, each quadrant of sprite

--- as4/src/as4.cpp
+++ as4/src/as4.cpp
@@ -47,14 +47,14 @@ int main() {
 
 
     // Game play and physics variables 
-    float speed = 19; // Initial speed 
+    float speed = 5; // Initial speed 
     int jump = 10; // Jump multiplier 
     float jumpSpeed = 20; // Initial jump velocity
     float gravity = 4.9;  // Gravity to bring character back to the ground
     bool isJumping = false; // Flag to track if the character is currently jumping
 
     // 0 , 0 is the top left of screen, y+ is down into the screen and X+ is to the right 
-    Vector2 position = {width / 2, 675};
+    Vector2 linkPos = {width / 2, 675};
     Vector2 rupeePos1 = {random1, -90};
     Vector2 rupeePos2 = {random2, -90};
     Vector2 rupeePos3 = {random3, -90};
@@ -80,60 +80,60 @@ int main() {
         Vector2 rupeePos = {random5, -90}; // Initializer for items in game 
         UpdateMusicStream(music);
 
-        // raylib::Vector2 velocity = {position.x * speed, position.y * speed *2};
+        // raylib::Vector2 velocity = {linkPos.x * speed, linkPos.y * speed *2};
 
         rupeePos1.y += gravity;
-        rupeePos2.y += gravity;
-        rupeePos3.y += gravity;
-        rupeePos4.y += gravity;
-        rupeePos5.y += gravity;
+        // rupeePos2.y += gravity;
+        // rupeePos3.y += gravity;
+        // rupeePos4.y += gravity;
+        // rupeePos5.y += gravity;
 
-        // Stop rupees when they reach y position 885
+        // Stop rupees when they reach y linkPos 885
         if (rupeePos1.y >= 875) rupeePos1.y = 880;
-        if (rupeePos2.y >= 875) rupeePos2.y = 880;
-        if (rupeePos3.y >= 875) rupeePos3.y = 880;
-        if (rupeePos4.y >= 875) rupeePos4.y = 880;
-        if (rupeePos5.y >= 875) rupeePos5.y = 880;
+        // if (rupeePos2.y >= 875) rupeePos2.y = 880;
+        // if (rupeePos3.y >= 875) rupeePos3.y = 880;
+        // if (rupeePos4.y >= 875) rupeePos4.y = 880;
+        // if (rupeePos5.y >= 875) rupeePos5.y = 880;
 
-        // Reset rupee positions if they are ±5 away from the initial position
-        // Check and reset rupee positions individually
+        // Reset rupee linkPoss if they are ±5 away from the initial linkPos
+        // Check and reset rupee linkPoss individually
         
-
-        // if (std::abs(rupeePos2.y - position.y) < 5 || std::abs(rupeePos2.x - position.x) < 5) {
+        if ((rupeePos1.y - linkPos.y) < 200 && (rupeePos1.x - linkPos.x) < 175) {
+            std::cout << "Rupee 1 linkPos reset!" << std::endl;
+            rupeePos1 = {static_cast<float>(rand() % 1856), -90};
+        }
+        // if (std::abs(rupeePos2.y - linkPos.y) < 5 || std::abs(rupeePos2.x - linkPos.x) < 5) {
         //     rupeePos2 = {static_cast<float>(rand() % 1856), -90};
         // }
 
-        // if (std::abs(rupeePos3.y - position.y) < 5 || std::abs(rupeePos3.x - position.x) < 5) {
+        // if (std::abs(rupeePos3.y - linkPos.y) < 5 || std::abs(rupeePos3.x - linkPos.x) < 5) {
         //     rupeePos3 = {static_cast<float>(rand() % 1856), -90};
         // }
 
-        // if (std::abs(rupeePos4.y - position.y) < 5 || std::abs(rupeePos4.x - position.x) < 5) {
+        // if (std::abs(rupeePos4.y - linkPos.y) < 5 || std::abs(rupeePos4.x - linkPos.x) < 5) {
         //     rupeePos4 = {static_cast<float>(rand() % 1856), -90};
         // }
 
-        // if (std::abs(rupeePos5.y - position.y) < 5 || std::abs(rupeePos5.x - position.x) < 5) {
+        // if (std::abs(rupeePos5.y - linkPos.y) < 5 || std::abs(rupeePos5.x - linkPos.x) < 5) {
         //     rupeePos5 = {static_cast<float>(rand() % 1856), -90};
         // }
 
 
         // Wall boundary 
-        if (position.x > 1775) {// Right boundary
+        if (linkPos.x > 1775) {// Right boundary
             PlaySound(naviWatch);
-            position.x = 1774;
+            linkPos.x = 1774;
         } 
-        else if (position.x < -190) {// Left boundary
+        else if (linkPos.x < -190) {// Left boundary
             PlaySound(naviWatch);
-            position.x = -189; 
+            linkPos.x = -189; 
         }
         
         
         window.BeginDrawing();
         {   
 
-            if (std::abs(rupeePos1.y - position.y) == 5 || std::abs(rupeePos1.x - position.x) == 5) {
-                std::cout << "Rupee 1 position reset!" << std::endl;
-                rupeePos1 = {static_cast<float>(rand() % 1856), -90};
-            }
+            
             window.ClearBackground(GREEN);
             // Background and foreground for link
             DrawTextureEx(wallpaper, (Vector2){0, -150}, 0, 2, YELLOW);
@@ -142,21 +142,21 @@ int main() {
             // Character creation
             // Rectangle linkSource = (Rectangle){0, 0, 350, 467};
             // Rectangle linkDest = (Rectangle){static_cast<float>(width / 2), 658, linkSource.width, linkSource.height};
-            // DrawTexturePro(characterLink, linkSource, linkDest, position, 0, WHITE);
-            DrawTextureEx(characterLink, position, 0, 1, WHITE);
+            // DrawTexturePro(characterLink, linkSource, linkDest, linkPos, 0, WHITE);
+            DrawTextureEx(characterLink, linkPos, 0, 1, WHITE);
 
             // Rupee Creation
             DrawTextureEx(Rupee, rupeePos1, 0, .5, RED);
-            DrawTextureEx(Rupee, rupeePos2, 0, .5, GREEN);
-            DrawTextureEx(Rupee, rupeePos3, 0, .5, GOLD);
-            DrawTextureEx(Rupee, rupeePos4, 0, .5, DARKPURPLE);
-            DrawTextureEx(Rupee, rupeePos5, 0, .5, DARKBLUE);
+            // DrawTextureEx(Rupee, rupeePos2, 0, .5, GREEN);
+            // DrawTextureEx(Rupee, rupeePos3, 0, .5, GOLD);
+            // DrawTextureEx(Rupee, rupeePos4, 0, .5, DARKPURPLE);
+            // DrawTextureEx(Rupee, rupeePos5, 0, .5, DARKBLUE);
 
 
             if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {speed = 10;} 
             else speed = 1;
-            if (IsKeyDown(KEY_D)) {position.x += 15 * speed;}
-            if (IsKeyDown(KEY_A)) {position.x -= 15 * speed;}
+            if (IsKeyDown(KEY_D)) {linkPos.x += 5 * speed;}
+            if (IsKeyDown(KEY_A)) {linkPos.x -= 5 * speed;}
             if (IsKeyPressed(KEY_W) && !isJumping) {
                 // Start jumping only if not already jumping
                 isJumping = true;
@@ -164,26 +164,27 @@ int main() {
             }
             // Jump physics
             if (isJumping) {
-                position.y -= 2.5;
+                linkPos.y -= 2.5;
             } 
-            else if (position.y <= 682 && !isJumping) {
-                position.y += 4;
+            else if (linkPos.y <= 682 && !isJumping) {
+                linkPos.y += 4;
             }
             // Check if the character has landed
-            if (position.y <= 535) {
+            if (linkPos.y <= 535) {
                 isJumping = false;
-            } else if (position.y >= 682) {
-                position.y = 682;
+            } else if (linkPos.y >= 682) {
+                linkPos.y = 682;
                 isJumping = false;
             }
             
             // Draw UI elements
             DrawFPS(10, 10);
-            text.Draw(std::to_string(position.y), (width / 2.5), height * .15, textSize * 1.25, raylib::Color::Black());
-            // text.Draw(std::to_string(position.x), (width / 2.5), height * .15, textSize * 1.25, raylib::Color::Black());
-            text.Draw(std::to_string(abs(rupeePos1.x - position.x)), (width / 1.5), height * .5, textSize * 1.25, raylib::Color::Black());
-            text.Draw(std::to_string(abs(rupeePos1.y - position.y)), (width / 2.5), height * .5, textSize * 1.25, raylib::Color::Black());
-            text.Draw(title, (width / 2.5), height * .25, textSize * 2.25, raylib::Color::Black());
+            text.Draw(std::to_string(linkPos.y), (width / 1.5), height * .15, textSize * 1.25, raylib::Color::Black());
+            text.Draw(std::to_string(linkPos.x), (width / 2.5), height * .15, textSize * 1.25, raylib::Color::Black());
+            // text.Draw(std::to_string(linkPos.x), (width / 2.5), height * .15, textSize * 1.25, raylib::Color::Black());
+            text.Draw(std::to_string(abs(rupeePos1.x - linkPos.x)), (width / 1.5), height * .5, textSize * 1.25, raylib::Color::Black());
+            text.Draw(std::to_string(abs(rupeePos1.y - linkPos.y)), (width / 2.5), height * .5, textSize * 1.25, raylib::Color::Black());
+            // text.Draw(title, (width / 2.5), height * .25, textSize * 2.25, raylib::Color::Black());
 
         }
         window.EndDrawing();
